d9510f05881a24c2cc20c66c243c7f097443c2a5
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-03-18 22:33:38 +0530

    adding food items in canteen

--- src/pages/Canteen/canFoodInfo.js
+++ src/pages/Canteen/canFoodInfo.js
@@ -1,14 +1,47 @@
 export const canFoods = [
     {
+        foodName: 'Coffee',
+        price: '15',
+        id: 'id-nhfn-e'
+    },
+    {
+        foodName: 'Tea',
+        price: '15',
+        id: 'id-z9of-e'
+    },
+    {
+        foodName: 'Breakfast',
+        price: '40',
+        id: 'id-m2yj-e'
+    },
+    {
+        foodName: 'Combo Breakfast',
+        price: '60',
+        id: 'id-dbr6-e'
+    },
+    {
         foodName: 'Meals',
-        price: '60'
+        price: '60',
+        id: 'id-6ml4-e',
     },
     {
         foodName: 'Chappati',
-        price: '40'
+        price: '40',
+        id: 'id-r4ux-e'
     },
     {
-        foodName: 'Curd Rice',
-        price: '40'
+        foodName: 'Rice Sambar',
+        price: '40',
+        id: 'id-k7lo-e'
+    },
+    {
+        foodName: 'Mudde Meals',
+        price: '60',
+        id: 'id-un9f-e'
+    },
+    {
+        foodName: 'ButterMilk',
+        price: '10',
+        id: 'id-fehy-e'
     },
 ]
