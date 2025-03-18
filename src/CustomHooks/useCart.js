635090d1220e2615d5e99204c937b1858cc966bd
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-03-18 22:34:08 +0530

    included add to cart feature for canteen

--- src/CustomHooks/useCart.js
+++ src/CustomHooks/useCart.js
@@ -1,6 +1,7 @@
 import CartContext from "../contexts/cart/CartContext";
 import { useContext } from 'react'
 import { cafeFoods } from "../pages/Cafeteria/cafeFoodInfo";
+import { canFoods } from "../pages/Canteen/canFoodInfo";
 
 function useCart() {
     const { cart, setCart } = useContext(CartContext);
@@ -14,11 +15,20 @@ function useCart() {
         });
     };
 
+    const addItemCan = (id) => {
+        const itemId = id;
+        canFoods.map((food) => {
+            if (food.id === itemId) {
+                setCart((prevCart) => [...prevCart, food]);
+            }
+        });
+    }
+
     const deleteItem = (id) => {
         setCart((prevCart) => prevCart.filter((item) => item.id !== id));
     }
 
-    return { cart, addItem, deleteItem };
+    return { cart, addItem, deleteItem, addItemCan };
 }
 
 export default useCart;
