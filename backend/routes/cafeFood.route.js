0afaf251abe89a333d9aca54efe0fb3e72523767
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-06-18 13:35:00 +0530

    post routes

--- backend/routes/cafeFood.route.js
+++ backend/routes/cafeFood.route.js
@@ -2,6 +2,10 @@ import Router from 'express'
 
 const cafeFoodRouter = Router();
 
+// this will be visible for admins -- create food through admin page
+cafeFoodRouter.post('/', (req, res) => res.send({ message: "create cafeteria food" }));
+
+// this will be visible for students
 cafeFoodRouter.get('/menu', (req, res) => res.send({ message: "showing cafe menu" }));
 
 export default cafeFoodRouter;
--- backend/routes/canteenFood.route.js
+++ backend/routes/canteenFood.route.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const canteenFoodRouter = Router();
 
+// this will be visible for admins -- create food through admin page
+canteenFoodRouter.post('/', (req, res) => res.send({ message: "create canteen food" }));
+
+// this will be visible for students
 canteenFoodRouter.get('/menu', (req, res) => res.send({ message: "showing canteen menu" }));
 
 export default canteenFoodRouter;
