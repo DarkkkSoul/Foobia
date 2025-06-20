57ac1b86c24d5915d52476afbd9a8e035f6c0858
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-06-20 23:47:25 +0530

    designing buttons of logout & cafe/canteen

--- frontend/src/pages/admin/AdminCafe.jsx
+++ frontend/src/pages/admin/AdminCafe.jsx
@@ -5,156 +5,157 @@ import { Link } from 'react-router';
 
 function AdminCafe() {
 
-    const navigate = useNavigate();
+   const navigate = useNavigate();
 
-    // add
-    const [foodName, setFoodName] = useState('');
-    const [price, setPrice] = useState('');
-    const [message, setMessage] = useState('');
+   // add
+   const [foodName, setFoodName] = useState('');
+   const [price, setPrice] = useState('');
+   const [message, setMessage] = useState('');
 
-    const handleCafeteriaSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/cafeteria/add`, {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ foodName, price })
+   const handleCafeteriaSubmit = async (e) => {
+      e.preventDefault();
+      try {
+         const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/cafeteria/add`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ foodName, price })
+         });
+
+         const data = await response.json();
+
+         if (response.ok) {
+            setMessage(data.message);
+            console.log(data.message);
+            setTimeout(() => {
+               setFoodName('');
+               setPrice('');
+            }, 1000);
+
+         } else {
+            setMessage(data.errorMessage);
+            console.log(data.errorMessage);
+            setTimeout(() => {
+               setFoodName('');
+               setPrice('');
+            }, 1000);
+         }
+
+
+      } catch (error) {
+         console.log("ERROR:", error);
+      }
+   }
+
+   useEffect(() => {
+      if (message) {
+         setTimeout(() => {
+            setMessage('');
+         }, 1500)
+      }
+   }, [message]);
+
+   // display 
+
+   const [cafeMenu, setCafeMenu] = useState([]);
+   const [errorMessage, setErrorMessage] = useState('');
+
+   useEffect(() => {
+      const displayCafeFood = async () => {
+         try {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/cafeteria/menu`, {
+               method: 'GET',
+               credentials: 'include'
             });
 
             const data = await response.json();
 
             if (response.ok) {
-                setMessage(data.message);
-                console.log(data.message);
-                setTimeout(() => {
-                    setFoodName('');
-                    setPrice('');
-                }, 1000);
-
+               setCafeMenu(data.menu.cafeMenu);
             } else {
-                setMessage(data.errorMessage);
-                console.log(data.errorMessage);
-                setTimeout(() => {
-                    setFoodName('');
-                    setPrice('');
-                }, 1000);
+               setErrorMessage(data.errorMessage);
             }
 
-
-        } catch (error) {
-            console.log("ERROR:", error);
-        }
-    }
-
-    useEffect(() => {
-        if (message) {
-            setTimeout(() => {
-                setMessage('');
-            }, 1500)
-        }
-    }, [message]);
-
-    // display 
-
-    const [cafeMenu, setCafeMenu] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    useEffect(() => {
-        const displayCafeFood = async () => {
-            try {
-                const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/cafeteria/menu`, {
-                    method: 'GET',
-                    credentials: 'include'
-                });
-
-                const data = await response.json();
-
-                if (response.ok) {
-                    setCafeMenu(data.menu.cafeMenu);
-                } else {
-                    setErrorMessage(data.errorMessage);
-                }
-
-            } catch (error) {
-                console.log('ERROR:', error);
-            }
-        }
-        displayCafeFood();
-    }, [message]);
-
-    const handleLogout = async () => {
-        try {
-            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/logout`, {
-                method: 'POST',
-                credentials: 'include'
-            });
-
-            if (response.ok) {
-                navigate('/auth');
-            }
-        } catch (error) {
+         } catch (error) {
             console.log('ERROR:', error);
-        }
+         }
+      }
+      displayCafeFood();
+   }, [message]);
 
-    }
+   const handleLogout = async () => {
+      try {
+         const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/logout`, {
+            method: 'POST',
+            credentials: 'include'
+         });
 
-    return (
-        <div className="min-h-screen flex gap-x-10 bg-gradient-to-br from-pink-300 via-fuchsia-500 to-purple-800 p-6">
+         if (response.ok) {
+            navigate('/auth');
+         }
+      } catch (error) {
+         console.log('ERROR:', error);
+      }
 
-            <div className="gap-y-10 items-center justify-center bg-gradient-to-br from-pink-200 via-fuchsia-300 to-purple-400 h-full p-6 rounded-md">
+   }
 
-                {/* First Form */}
-                <div className="w-full max-w-md bg-white rounded-xl shadow-md p-8">
-                    <h2 className="text-xl font-semibold text-purple-800 mb-6 text-center">Cafeteria</h2>
-                    <form className="flex flex-col gap-4" onSubmit={handleCafeteriaSubmit}>
-                        <div className="flex flex-col">
-                            <label className="text-sm font-medium text-gray-700">Item Name</label>
-                            <input
-                                type="text"
-                                placeholder="Enter Food Name"
-                                className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                                value={foodName}
-                                onChange={(e) => setFoodName(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="flex flex-col">
-                            <label className="text-sm font-medium text-gray-700">Price</label>
-                            <input
-                                type="text"
-                                placeholder="Enter Price"
-                                className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                                value={price}
-                                onChange={(e) => setPrice(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="mt-4 bg-purple-700 text-white py-2 rounded-md hover:bg-purple-800 transition duration-300"
-                        >
-                            Create
-                        </button>
-                    </form>
-                    {message && <p className="text-purple-700 mt-4 text-center text-xs">{message}</p>}
-                </div>
+   return (
+      <div className="min-h-screen flex gap-x-10 bg-gradient-to-br from-pink-300 via-fuchsia-500 to-purple-800 p-6">
 
-                <button onClick={handleLogout}>Logout</button>
-                <Link to='/admin/canteen'>Canteen</Link>
+         <div className="gap-y-10 flex flex-col bg-gradient-to-br from-pink-200 via-fuchsia-300 to-purple-400 h-full p-6 rounded-md">
+
+            <div className="w-full max-w-md bg-white rounded-xl shadow-md p-8">
+               <h2 className="text-xl font-semibold text-purple-800 mb-6 text-center">Cafeteria</h2>
+               <form className="flex flex-col gap-4" onSubmit={handleCafeteriaSubmit}>
+                  <div className="flex flex-col">
+                     <label className="text-sm font-medium text-gray-700">Item Name</label>
+                     <input
+                        type="text"
+                        placeholder="Enter Food Name"
+                        className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                        value={foodName}
+                        onChange={(e) => setFoodName(e.target.value)}
+                        required
+                     />
+                  </div>
+                  <div className="flex flex-col">
+                     <label className="text-sm font-medium text-gray-700">Price</label>
+                     <input
+                        type="text"
+                        placeholder="Enter Price"
+                        className="mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        required
+                     />
+                  </div>
+                  <button
+                     type="submit"
+                     className="mt-4 bg-gradient-to-r from-purple-500 to-purple-900 text-white py-2 font-semibold tracking-wider rounded-md hover:bg-purple-800 duration-300 cursor-pointer active:scale-95 transition-all"
+                  >
+                     Create
+                  </button>
+               </form>
+               {message && <p className="text-purple-700 mt-4 text-center text-xs">{message}</p>}
             </div>
 
-            <div className='w-full bg-gradient-to-br from-pink-200 via-fuchsia-300 to-purple-400 rounded-md p-5 grid grid-cols-5 grid-rows-8 gap-4'>
-                {cafeMenu.map((item) => (
-                    <FoodItem key={item._id} foodName={item.foodName} price={item.price} />
-                ))}
-                {errorMessage && <p className="text-purple-700 mt-4 text-center text-xs">{errorMessage}</p>}
+            <div className='flex flex-row items-center justify-around'>
+               <button onClick={handleLogout} className='bg-gradient-to-r from-red-600 to-orange-400 px-6 py-2.5 rounded-xl text-white font-semibold hover:scale-105 active:scale-95 transition-all cursor-pointer tracking-wide'>Logout</button>
+               <Link to='/admin/canteen' className='bg-gradient-to-r from-fuchsia-600 to-pink-600 px-7 py-2.5 rounded-xl text-white font-semibold hover:scale-105 active:scale-95 transition-all cursor-pointer tracking-wide'>Canteen</Link>
             </div>
+         </div>
 
-        </div>
-    )
+         <div className='w-full bg-gradient-to-br from-pink-200 via-fuchsia-300 to-purple-400 rounded-md p-5 grid grid-cols-5 grid-rows-8 gap-4'>
+            {cafeMenu.map((item) => (
+               <FoodItem key={item._id} foodName={item.foodName} price={item.price} />
+            ))}
+            {errorMessage && <p className="text-purple-700 mt-4 text-center text-xs">{errorMessage}</p>}
+         </div>
+
+      </div>
+   )
 }
 
 export default AdminCafe
